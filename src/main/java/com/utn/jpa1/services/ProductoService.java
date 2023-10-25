7e743fd9f8d32e6660195db23244c8b48483515a
titovaieretti <rcvaieretti@gmail.com>
2023-10-25 17:00:20 -0300

    error findby

--- src/main/java/com/utn/jpa1/services/ProductoService.java
+++ src/main/java/com/utn/jpa1/services/ProductoService.java
@@ -1,6 +1,12 @@
 package com.utn.jpa1.services;
 
 import com.utn.jpa1.entities.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ProductoService extends  BaseService<Producto, Long> {
+    List<Producto> search(String filtro) throws Exception;
+    Page<Producto> search(String filtro, Pageable pageable) throws Exception;
 }

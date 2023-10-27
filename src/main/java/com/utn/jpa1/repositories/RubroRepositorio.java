f12d715a1022af1606502588238b8a540d1a1f0c
titovaieretti <rcvaieretti@gmail.com>
2023-10-27 09:43:58 -0300

    rubros

--- src/main/java/com/utn/jpa1/repositories/RubroRepositorio.java
+++ src/main/java/com/utn/jpa1/repositories/RubroRepositorio.java
@@ -1,11 +1,9 @@
 package com.utn.jpa1.repositories;
 
 
-import com.utn.jpa1.entities.Producto;
 import com.utn.jpa1.entities.Rubro;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,12 +11,13 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RubroRepositorio extends JpaRepository<Rubro, Long> {
+public interface RubroRepositorio extends BaseRepository<Rubro, Long> {
     List<Rubro> findByDenominacionContaining(String denominacion);
     Page<Rubro> findByDenominacionContaining(String denominacion, Pageable pageable);
 
-    @Query(value = "SELECT c FROM Rubro c WHERE c.denominacion LIKE %:filtro%")
+    @Query(value = "SELECT r FROM Rubro r WHERE r.denominacion LIKE %:filtro%")
     List<Rubro> search(@Param("filtro") String filtro);
-    @Query(value = "SELECT c FROM Rubro c WHERE c.denominacion LIKE %:filtro%")
+    @Query(value = "SELECT r FROM Rubro r WHERE r.denominacion LIKE %:filtro%")
     Page<Rubro> search(@Param("filtro") String filtro, Pageable pageable);
+
 }
--- src/main/java/com/utn/jpa1/services/RubroServiceImpl.java
+++ src/main/java/com/utn/jpa1/services/RubroServiceImpl.java
@@ -1,21 +1,23 @@
 package com.utn.jpa1.services;
 
-import com.utn.jpa1.entities.Producto;
 import com.utn.jpa1.entities.Rubro;
 import com.utn.jpa1.repositories.BaseRepository;
 import com.utn.jpa1.repositories.RubroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class RubroServiceImpl extends BaseServiceImpl<Rubro,Long> implements RubroService {
 
+    @Autowired
     private final RubroRepositorio rubroRepositorio;
 
     public RubroServiceImpl(BaseRepository<Rubro, Long> baseRepository, RubroRepositorio rubroRepositorio) {
         super(baseRepository);
-        this.rubroRepositorio=rubroRepositorio;
+        this.rubroRepositorio = rubroRepositorio;
     }
 
     @Override
@@ -35,5 +37,6 @@ public class RubroServiceImpl extends BaseServiceImpl<Rubro,Long> implements Rub
         } catch (Exception e){
             throw new Exception(e.getMessage());
         }
+
     }
 }
